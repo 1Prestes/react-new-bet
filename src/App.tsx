26b976c72b8dc7e293a6ddef2299cfa65c3a7b76
Dijango Alves <dijango_ilha@hotmail.com>
2021-04-12 09:43:01 -0300

    style: add reset css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './assets/styles/global-styles'
+import ResetCSS from './assets/styles/reset-css'
 import theme from './assets/styles/theme'
 import SignIn from './Pages/Sign-in'
 import Routes from './routes'
@@ -9,6 +10,7 @@ import Routes from './routes'
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
+      <ResetCSS />
       <GlobalStyles />
       <Routes>
         <SignIn />
